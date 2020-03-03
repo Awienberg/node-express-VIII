cf163c1c11a7cb6312da1947e10fb7b2cf83dbb8
Awienberg <a.wienberg@hotmail.com>
2020-03-03 09:39:27 +0100

    data om danmark

--- 0/routes/index.js
+++ 0/routes/index.js
@@ -15,7 +15,7 @@ router.get('/country', function(req, res, next) {
 /* /country:country */
 router.get("/country/:country", function(req, res, next) {  // route with country name
   worldControllers.retrieveCountry(req, res, next);  // get data from
-  res.render('chosenCountry', { title: 'Chosen Country' });
+  res.render('chosenCountry', { title: 'Chosen Country', name: 'Danmark', continent: 'Europa', area: '43.094', popu: '5.869.410', run: 'Monarki'  });
 });
 
 module.exports = router;
